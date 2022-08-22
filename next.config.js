4b783aef4d4f9a870c91e5bfa98ce0bb237cc3f7
nimishjn <nimishjain100701@gmail.com>
2022-08-22 09:57:34 +0530

    Refactor: Added fetch logs

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
-	async redirects() {
-		return [
-			{
-				source: '/',
-				destination:
-					'https://drive.google.com/uc?export=view&id=1Tg31PggA8KJE4EIhHue5fIBz3qskzfgF',
-				permanent: true,
-			},
-		];
-	},
 };
 
 module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -7,10 +7,32 @@ export default function Home() {
 			</Head>
 			<main>
 				If it does not redirect,{' '}
-				<a href='https://drive.google.com/uc?export=view&id=1Tg31PggA8KJE4EIhHue5fIBz3qskzfgF'>
+				<a
+					href={
+						process.env.NEXT_PUBLIC_RESUME_LINK ||
+						'https://drive.google.com/uc?export=view&id=1Tg31PggA8KJE4EIhHue5fIBz3qskzfgF'
+					}
+				>
 					Click here!
 				</a>
 			</main>
 		</div>
 	);
+}
+
+export async function getStaticProps(context) {
+	const date = new Date();
+	console.log(
+		'Resume fetched at',
+		date.toLocaleString(),
+		date.getTimezoneOffset()
+	);
+	return {
+		redirect: {
+			destination:
+				process.env.RESUME_LINK ||
+				'https://drive.google.com/uc?export=view&id=1Tg31PggA8KJE4EIhHue5fIBz3qskzfgF',
+			permanent: false,
+		},
+	};
 }
